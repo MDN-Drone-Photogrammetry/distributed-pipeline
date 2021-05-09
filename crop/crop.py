1fe07e966fdc11ca027c034d18a95bbc4e2619d6
yusuke <ymiy0005@student.monash.edu>
2021-05-09 13:09:55 +1000

    crop boundary set

--- crop/crop.py
+++ crop/crop.py
@@ -12,7 +12,7 @@ def crop(files):
         if file.endswith('.laz'):
             result = subprocess.check_output(["pdal", "info", file])
             #find minx
-            start = str(result).find('minx')
+            start = str(result).rfind('minx')
             if start == -1:
                 print('Count not found minx in {file}')
             else:
@@ -20,7 +20,7 @@ def crop(files):
                 minx = str(result)[start+6:end]
 
                 #find maxx
-                start = str(result).find('maxx')
+                start = str(result).rfind('maxx')
                 if start == -1:
                     print('Count not found maxx in {file}')
                 else:
@@ -28,29 +28,69 @@ def crop(files):
                     maxx = str(result)[start+6:end]
 
                 #find miny
-                start = str(result).find('miny')
+                start = str(result).rfind('miny')
                 if start == -1:
                     print('Count not found miny in {file}')
                 else:
                     end = str(result).find(',', start)
                     miny = str(result)[start+6:end]
-                print(f"minx is {float(maxx)}")
-                print(f"maxx is {float(minx)}")
-                length = float(maxx) - float(minx)
+                
+                #find maxy
+                start = str(result).rfind('maxy')
+                if start == -1:
+                    print('Count not found maxy in {file}')
+                else:
+                    end = str(result).find(',', start)
+                    maxy = str(result)[start+6:end]
+
+                #find minz
+                start = str(result).rfind('minz')
+                if start == -1:
+                    print('Count not found minz in {file}')
+                else:
+                    end = str(result).find('\\n', start)
+                    minz = str(result)[start+6:end]
+
+                #find maxz
+                start = str(result).rfind('maxz')
+                if start == -1:
+                    print('Count not found maxz in {file}')
+                else:
+                    end = str(result).find(',', start)
+                    maxz = str(result)[start+6:end]
+
+                print(f"minx is {float(minx)}")
+                print(f"maxx is {float(maxx)}")
+                print(f"miny is {float(miny)}")
+                print(f"maxy is {float(maxy)}")
+                print(f"minz is {float(minz)}")
+                print(f"maxz is {float(maxz)}")
+                lengthX = float(maxx) - float(minx)
+                lengthY = float(maxy) - float(miny)
+                lengthZ = float(maxz) - float(minz)
+                lengthMax = 0
+                # check which axis of the point cloud has the longest length
+                if lengthX > lengthY and lengthX > lengthZ:
+                    lengthMax = lengthX
+                elif lengthY > lengthZ:
+                    lengthMax = lengthY 
+                else:
+                    lengthMax = lengthZ
+
                 # crop
-                new_length = length*(10/11)
-                print(f"{file} had x-axis length{length}, this is cropped to {new_length}")
+                new_length = lengthMax*(10/11)
+                print(f"Point cloud in {file} has length{lengthMax}, this is cropped to {new_length}")
 
             #replace strings in crop.json
             for file2 in files:
 
-                if file2 == 'crop.json':
+                if file2 == 'cropBound.json':
 
                     #subprocess.run(["sed", f"'s/1/{str(minx)} {str(miny)}/g'", file])
                     #subprocess.run(["sed", "-e", "s/1/" + "POINT(" + str(minx) + " " + str(miny) + ")/g", file, "&&", "sed", "-e", "s/2/" + str(new_length) + "/g" , file, "&&", "sed", "-e", "s/0/" + file + "/g", file])
-                    subprocess.run(["sed", "-i", "s/one/" + "POINT(" + str(minx) + " " + str(miny) + ")/", file2])
-                    subprocess.run(["sed", "-i", "s/two/" + str(new_length) + "/", file2])
                     subprocess.run(["sed", "-i", "s/zero/" + file + "/", file2])
+                    subprocess.run(["sed", "-i", "s/one/"+"["+str(minx)+","+str(maxx)+"],["+str(miny)+","+str(maxy)+"],["+str(minz)+","+str(minz)+"])/", file2])
+                    
                     print(file2 + ":")
                     subprocess.run(["cat", file2])
 
@@ -59,7 +99,7 @@ def crop(files):
                     print("pdal crop done")
 
                     #recreate crop.json to be back at original state
-                    subprocess.run(["rm", "crop.json"])
-                    subprocess.run(["cp", "cropbackup.json", "crop.json"])
+                    subprocess.run(["rm", "cropBound.json"])
+                    subprocess.run(["cp", "cropBoundbackup.json", "cropBound.json"])
 
-crop(['test.laz', 'crop.json'])
+crop(['test.laz', 'cropBound.json'])
